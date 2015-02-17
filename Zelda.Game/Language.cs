dfb689913c0dcfd522791b93887e50a2c6c4d5b9
likeleon <likeleon@gmail.com>
2015-02-17 18:49:05 +0900

    누락 파일

---
+++ Zelda.Game/Language.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Zelda.Game
+{
+    static class Language
+    {
+        static string _languageCode;
+        public static string LanguageCode
+        {
+            get { return _languageCode; }
+            set
+            {
+                if (!HasLanguage(value))
+                    throw new Exception("No such language: '" + value + "'");
+
+                _languageCode = value;
+            }
+        }
+
+        public static bool HasLanguage(string languageCode)
+        {
+            return CurrentMod.ResourceExists(ResourceType.Language, languageCode);
+        }
+
+        public static string GetLanguageName(string languageCode)
+        {
+            var languages = CurrentMod.GetResources(ResourceType.Language);
+            string name;
+            if (languages.TryGetValue(languageCode, out name))
+                return name;
+            return string.Empty;
+        }
+    }
+}
---
+++ Zelda.Game/Script/Language.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LanguageInternal = Zelda.Game.Language;
+
+namespace Zelda.Game.Script
+{
+    public static class Language
+    {
+        public static string LanguageCode
+        {
+            get { return LanguageInternal.LanguageCode; }
+            set { LanguageInternal.LanguageCode = value; }
+        }
+
+        public static string GetLanguageName(string languageCode)
+        {
+            if (languageCode != null)
+            {
+                if (!LanguageInternal.HasLanguage(languageCode))
+                    throw new Exception("No such language: '" + languageCode + "'");
+                
+                return LanguageInternal.GetLanguageName(languageCode);
+            }
+            else
+            {
+                if (LanguageInternal.LanguageCode == null)
+                    throw new Exception("No language is set");
+
+                return LanguageInternal.GetLanguageName(LanguageInternal.LanguageCode);
+            }
+        }
+    }
+}
