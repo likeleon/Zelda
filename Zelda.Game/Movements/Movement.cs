9278fc359ed06b15bdfe402a371614d3c5614fdb
likeleon <likeleon@gmail.com>
2015-03-08 19:12:11 +0900

    Movement 중간 서밋

---
+++ Zelda.Game/Movements/Movement.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Zelda.Game.Movements
+{
+    class Movement
+    {
+        bool _isScriptCallbackEnable;
+        public bool IsScriptCallbackEnable
+        {
+            get { return _isScriptCallbackEnable; }
+            set { _isScriptCallbackEnable = value; }
+        }
+    }
+}
---
+++ Zelda.Game/Script/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using RawMovement = Zelda.Game.Movements.Movement;
+
+namespace Zelda.Game.Script
+{
+    public class Movement
+    {
+        readonly RawMovement _rawMovement;
+
+        public static Movement Create(MovementType type)
+        {
+            RawMovement movement = null;
+            if (type == MovementType.Target)
+            {
+                Game game = ScriptContext.MainLoop.Game;
+                if (game != null)
+                    throw new NotImplementedException("If we are on a map, the default target should be the hero.");
+                else
+                    movement = new TargetMovement(null, 0, 0, 32, false);
+            }
+            else
+            {
+                string enumNames = String.Join(", ", Enum.GetNames(typeof(MovementType)));
+                throw new ArgumentOutOfRangeException("type", "should be one of: {0}".F(enumNames));
+            }
+
+            movement.IsScriptCallbackEnable = true;
+            return new Movement(movement);
+        }
+
+        Movement(RawMovement rawMovement)
+        {
+            _rawMovement = rawMovement;
+        }
+    }
+}
