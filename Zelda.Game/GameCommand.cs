ef2806f61718c3b0477dcca6e894ce5385f1b483
likeleon <likeleon@gmail.com>
2015-04-16 23:35:02 +0900

    입력 처리 시작... 졸림

---
+++ Zelda.Game/GameCommand.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Zelda.Game
+{
+    // 엔진에서 처리할 수 있는 내장된 커맨드들로 키보드나 조이패드와 매핑됩니다
+    enum GameCommand
+    {
+        None = -1,
+        Action,
+        Attack,
+        Item1,
+        Item2,
+        Pause,
+        Right,
+        Up,
+        Left,
+        Down
+    }
+}
---
+++ Zelda.Game/GameCommands.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Zelda.Game
+{
+    // 인게임 하이레벨 커맨드들과 그들의 키보드나 조이패드 매핑을 저장합니다.
+    // 이 클래스는 키보드나 조이패드의 로우 레벨 이벤트를 받아 내장된 게임 커맨드들이 눌렸거나 릴리즈되었음을
+    // 적당한 객체에게 알려주는 역할을 합니다.
+    class GameCommands
+    {
+        readonly Game _game;
+
+        public GameCommands(Game game)
+        {
+            _game = game;
+        }
+    }
+}
