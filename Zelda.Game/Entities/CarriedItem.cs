2826d35c49a7b3b842ba93b09e8d6ad5d6744652
likeleon <likeleon@gmail.com>
2015-05-04 20:40:02 +0900

    아이템 던질 때 그림자 그

--- Zelda.Game/Entities/CarriedItem.cs
+++ Zelda.Game/Entities/CarriedItem.cs
@@ -45,6 +45,9 @@ namespace Zelda.Game.Entities
             CreateSprite(animationSetId);
             Sprite.SetCurrentAnimation("stopped");
             SetMovement(movement);
+
+            _shadowSprite = new Sprite("entities/shadow");
+            _shadowSprite.SetCurrentAnimation("big");
         }
 
         public override EntityType Type
@@ -61,61 +64,18 @@ namespace Zelda.Game.Entities
         readonly Hero _hero;
         #endregion
 
-        #region 상태
+        #region 둘기
         public bool IsBeingLifted { get; private set; }
-        public bool IsBeingThrown { get; private set; }
         #endregion
 
-        public override void Update()
-        {
-            base.Update();
-
-            if (IsSuspended)
-                return;
-
-            if (IsBeingLifted && Movement.IsFinished)
-            {
-                IsBeingLifted = false;
-
-                ClearMovement();
-                SetMovement(new FollowMovement(_hero, 0, -18, true));
-            }
-
-            if (IsBeingThrown)
-            {
-                if (IsBroken)
-                    RemoveFromMap();
-                else if (Movement.IsStopped || _yIncrement >= 7)
-                    BreakItemOnGround();
-                else
-                {
-                    uint now = EngineSystem.Now;
-                    while (now >= _nextDownDate)
-                    {
-                        _nextDownDate += 40;
-                        _itemHeight -= _yIncrement;
-                        ++_yIncrement;
-                    }
-                }
-            }
-        }
-
-        public void SetAnimationStopped()
-        {
-            if (!IsBeingLifted && !IsBeingThrown)
-                Sprite.SetCurrentAnimation("stopped");
-        }
-
-        public void SetAnimationWalking()
-        {
-            if (!IsBeingLifted && !IsBeingThrown)
-                Sprite.SetCurrentAnimation("walking");
-        }
+        #region 던지기
+        public bool IsBeingThrown { get; private set; }
 
         Direction4 _throwingDirection = Direction4.Right;
         int _yIncrement;
         uint _nextDownDate;
         int _itemHeight;
+        Sprite _shadowSprite;
 
         public void ThrowItem(Direction4 direction)
         {
@@ -136,7 +96,9 @@ namespace Zelda.Game.Entities
             _nextDownDate = EngineSystem.Now + 40;
             _itemHeight = 18;
         }
+        #endregion
 
+        #region 파괴
         bool _isBreaking;
         public bool IsBroken
         {
@@ -158,11 +120,11 @@ namespace Zelda.Game.Entities
                         BreakItemOnGround();
                     }
                     break;
-                
+
                 case Ground.Hole:
                     RemoveFromMap();
                     break;
-                
+
                 case Ground.DeepWater:
                 case Ground.Lava:
                     RemoveFromMap();
@@ -194,6 +156,56 @@ namespace Zelda.Game.Entities
             _isBreaking = true;
         }
 
+        #endregion
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (IsSuspended)
+                return;
+
+            if (IsBeingLifted && Movement.IsFinished)
+            {
+                IsBeingLifted = false;
+
+                ClearMovement();
+                SetMovement(new FollowMovement(_hero, 0, -18, true));
+            }
+
+            if (IsBeingThrown)
+            {
+                _shadowSprite.Update();
+
+                if (IsBroken)
+                    RemoveFromMap();
+                else if (Movement.IsStopped || _yIncrement >= 7)
+                    BreakItemOnGround();
+                else
+                {
+                    uint now = EngineSystem.Now;
+                    while (now >= _nextDownDate)
+                    {
+                        _nextDownDate += 40;
+                        _itemHeight -= _yIncrement;
+                        ++_yIncrement;
+                    }
+                }
+            }
+        }
+
+        public void SetAnimationStopped()
+        {
+            if (!IsBeingLifted && !IsBeingThrown)
+                Sprite.SetCurrentAnimation("stopped");
+        }
+
+        public void SetAnimationWalking()
+        {
+            if (!IsBeingLifted && !IsBeingThrown)
+                Sprite.SetCurrentAnimation("walking");
+        }
+
         public override void DrawOnMap()
         {
             if (!IsDrawn())
@@ -202,12 +214,18 @@ namespace Zelda.Game.Entities
             if (!IsBeingThrown)
                 base.DrawOnMap();
             else
+            {
+                Map.DrawSprite(_shadowSprite, XY);
                 Map.DrawSprite(Sprite, X, Y - _itemHeight);
+            }
         }
 
         public override void SetSuspended(bool suspended)
         {
             base.SetSuspended(suspended);
+
+            if (IsBeingThrown)
+                _shadowSprite.SetSuspended(suspended);
 
             if (!suspended && WhenSuspended != 0)
             {
