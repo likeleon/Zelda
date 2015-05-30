2fc4e793d86f31ca6ef66b7fa53b1c760f01bc7c
likeleon <likeleon@gmail.com>
2015-05-31 06:17:03 +0900

    새 MOD 추가 (alttp: A Link to the Past)

---
+++ mods/alttp/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Alttp
+{
+    public class Main : Zelda.Game.Script.Main
+    {
+        protected override void OnStarted()
+        {
+            Console.WriteLine("A Link To The Past.");
+        }
+    }
+}
