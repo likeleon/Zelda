a214b81c5d16c4dee90c6222447ffe9f093badbe
Sung Ho Kim <likeleon@gmail.com>
2015-10-22 09:37:57 +0900

    Undo/Redo 툴바에 추가

---
+++ Zelda.Editor/Modules/UndoRedo/ToolBarDefinitions.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.Composition;
+using Zelda.Editor.Core.ToolBars;
+using Zelda.Editor.Modules.UndoRedo.Commands;
+
+namespace Zelda.Editor.Modules.UndoRedo
+{
+    static class ToolBarDefinitions
+    {
+        [Export]
+        public static ToolBarItemGroupDefinition StandardUndoRedoToolBarGroup = new ToolBarItemGroupDefinition(
+            ToolBars.ToolBarDefinitions.StandardToolBar, 10);
+
+        [Export]
+        public static ToolBarItemDefinition UndoToolBarItem = new CommandToolBarItemDefinition<UndoCommandDefinition>(
+            StandardUndoRedoToolBarGroup, 0);
+
+        [Export]
+        public static ToolBarItemDefinition RedoToolBarItem = new CommandToolBarItemDefinition<RedoCommandDefinition>(
+            StandardUndoRedoToolBarGroup, 1);
+    }
+}
