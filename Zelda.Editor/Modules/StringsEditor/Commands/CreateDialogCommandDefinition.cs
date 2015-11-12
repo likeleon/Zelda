6013a7b9740db45158caccd52e330399a69cba53
Sung Ho Kim <likeleon@gmail.com>
2015-11-13 01:19:47 +0900

    누락 파일

---
+++ Zelda.Editor/Modules/StringsEditor/Commands/CreateDialogCommandDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Zelda.Editor.Core.Commands;
+using Zelda.Editor.Core.Services;
+
+namespace Zelda.Editor.Modules.StringsEditor.Commands
+{
+    [CommandDefinition]
+    class CreateStringCommandDefinition : CommandDefinition
+    {
+        public const string CommandName = "StringsEditor.CreateString";
+        public override string Name { get { return CommandName; } }
+        public override string Text { get { return "New string..."; } }
+        public override string ToolTip { get { return "New string"; } }
+        public override Uri IconSource { get { return "icon_add.png".ToIconUri(); } }
+    }
+}
