2617c74c442c9ff2ff561f5d9a48c0fbb6f215e4
Sung Ho Kim <likeleon@gmail.com>
2016-06-06 13:25:20 +0900

    SpriteData도 XmlData 안 씀

--- Zelda.Game/SpriteAnimationSet.cs
+++ Zelda.Game/SpriteAnimationSet.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Zelda.Game.LowLevel;
 using Zelda.Game.Entities;
+using Zelda.Game.LowLevel;
 
 namespace Zelda.Game
 {
@@ -28,23 +28,18 @@ namespace Zelda.Game
 
         void Load()
         {
-            Debug.CheckAssertion(_animations.Count <= 0, "Animation set already loaded");
+            if (_animations.Count > 0)
+                throw new InvalidOperationException("Animation set already loaded");
 
-            string fileName = "sprites/" + _id + ".xml";
-            SpriteData data = new SpriteData();
-            bool success = data.ImportFromModFile(Core.Mod.ModFiles, fileName);
-            if (success)
-            {
-                DefaultAnimation = data.DefaultAnimationName;
-                foreach (var kvp in data.Animations)
-                    AddAnimation(kvp.Key, kvp.Value);
-            }
+            var fileName = "sprites/" + _id + ".xml";
+            var data = XmlLoader.Load<SpriteData>(Core.Mod.ModFiles, fileName);
+            DefaultAnimation = data.DefaultAnimationName;
+            data.Animations.Do(x => AddAnimation(x.Key, x.Value));
         }
 
         public void SetTileset(Tileset tileset)
         {
-            foreach (SpriteAnimation animation in _animations.Values)
-                animation.SetTileset(tileset);
+            _animations.Values.Do(a => a.SetTileset(tileset));
         }
 
         public bool HasAnimation(string animationName)
@@ -54,35 +49,28 @@ namespace Zelda.Game
 
         public SpriteAnimation GetAnimation(string animationName)
         {
-            Debug.CheckAssertion(HasAnimation(animationName),
-                "No animation '{0}' in animation set '{1}'".F(animationName, _id));
+            if (!HasAnimation(animationName))
+                throw new Exception("No animation '{0}' in animation set '{1}'".F(animationName, _id));
 
             return _animations[animationName];
         }
 
-        void AddAnimation(string animationName, SpriteAnimationData animationData)
+        void AddAnimation(string name, SpriteAnimationData a)
         {
-            string srcImage = animationData.SrcImage;
-            int frameDelay = animationData.FrameDelay;
-            int frameToLoopOn = animationData.LoopOnFrame;
-            List<SpriteAnimationDirection> directions = new List<SpriteAnimationDirection>();
-
-            foreach (var direction in animationData.Directions)
+            var directions = new List<SpriteAnimationDirection>();
+            foreach (var direction in a.Directions)
             {
                 MaxSize = new Size(Math.Max(direction.Size.Width, MaxSize.Width), Math.Max(direction.Size.Height, MaxSize.Height));
                 directions.Add(new SpriteAnimationDirection(direction.GetAllFrames().ToArray(), direction.Origin));
             }
 
-            _animations.Add(animationName, new SpriteAnimation(srcImage, directions.ToArray(), frameDelay, frameToLoopOn));
+            _animations.Add(name, new SpriteAnimation(a.SrcImage, directions.ToArray(), a.FrameDelay, a.LoopOnFrame));
         }
 
         public void EnablePixelCollisions()
         {
             if (!ArePixelCollisionsEnabled())
-            {
-                foreach (SpriteAnimation animation in _animations.Values)
-                    animation.EnablePixelCollisions();
-            }
+                _animations.Values.Do(a => a.EnablePixelCollisions());
         }
 
         public bool ArePixelCollisionsEnabled()
--- Zelda.Game/SpriteData.cs
+++ Zelda.Game/SpriteData.cs
@@ -1,40 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Xml.Serialization;
 using Zelda.Game.LowLevel;
 
 namespace Zelda.Game
 {
-    class SpriteAnimationDirectionData
+    public class SpriteAnimationDirectionData : IXmlDeserialized
     {
-        public Point XY { get; private set; }
-        public Size Size { get; private set; }
-        public Point Origin { get; private set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public int FrameWidth { get; set; }
+        public int FrameHeight { get; set; }
+
+        [DefaultValue(0)]
+        public int OriginX { get; set; } = 0;
+        [DefaultValue(0)]
+        public int OriginY { get; set; } = 0;
+
+        [XmlIgnore] public Point XY { get; private set; }
+        [XmlIgnore] public Size Size { get; private set; }
+        [XmlIgnore] public Point Origin { get; private set; }
         
+        [DefaultValue(1)]
         public int NumFrames
         {
             get { return _numFrames; }
             set { _numFrames = Math.Max(value, 1); }
         }
 
+        [DefaultValue(1)]
         public int NumColumns
         {
             get { return _numColumns; }
             set { _numColumns = Math.Max(value, 1); }
         }
 
-        int _numFrames;
-        int _numColumns;
-
-        public SpriteAnimationDirectionData(Point xy, Size size, Point origin, int numFrames, int numColumns = 1)
-        {
-            XY = xy;
-            Size = size;
-            Origin = origin;
-            NumFrames = numFrames;
-            NumColumns = numColumns;
-        }
+        int _numFrames = 1;
+        int _numColumns = 0;
 
         public List<Rectangle> GetAllFrames()
         {
@@ -46,141 +52,81 @@ namespace Zelda.Game
                 ++numRows;
 
             for (var row = 0; row < numRows && frameNumber < _numFrames; ++row)
-            {
                 for (var col = 0; col < _numColumns && frameNumber < NumFrames; ++col)
-                {
-                    frames.Add(new Rectangle(
-                        XY.X + col * Size.Width,
-                        XY.Y + row * Size.Height,
-                        Size.Width,
-                        Size.Height));
-                }
-            }
+                    frames.Add(new Rectangle(XY.X + col * Size.Width, XY.Y + row * Size.Height, Size.Width, Size.Height));
 
             return frames;
         }
-    }
 
-    class SpriteAnimationData
-    {
-        public string SrcImage { get; }
-        public bool SrcImageIsTileset { get { return SrcImage == "tileset"; } }
-        public int FrameDelay { get; }
-        public int LoopOnFrame { get; }
-        public IEnumerable<SpriteAnimationDirectionData> Directions { get; }
-
-        public SpriteAnimationData(string srcImage, List<SpriteAnimationDirectionData> directions, int frameDelay, int loopOnFrame)
+        public void OnDeserialized()
         {
-            SrcImage = srcImage;
-            Directions = directions;
-            FrameDelay = frameDelay;
-            LoopOnFrame = loopOnFrame;
+            if (NumColumns == 0)
+                NumColumns = NumFrames;
+
+            if (NumColumns < 1 || NumColumns > NumFrames)
+                throw new InvalidDataException("Bad field 'NumColumns': must be between 1 and the number of frames");
+
+            XY = new Point(X, Y);
+            Size = new Size(FrameWidth, FrameHeight);
+            Origin = new Point(OriginX, OriginY);
         }
     }
 
-    class SpriteData : XmlData
+    public class SpriteAnimationData : IXmlDeserialized
     {
-        public IReadOnlyDictionary<string, SpriteAnimationData> Animations { get { return _animations; } }
-        public string DefaultAnimationName { get; private set; }
+        [XmlAttribute]
+        public string Name { get; set; }
+        public string SrcImage { get; set; }
 
-        readonly Dictionary<string, SpriteAnimationData> _animations = new Dictionary<string, SpriteAnimationData>();
+        [XmlIgnore]
+        public bool SrcImageIsTileset => SrcImage == "tileset";
 
-        protected override bool OnImportFromBuffer(byte[] buffer)
+        [DefaultValue(0)]
+        public int FrameDelay { get; set; } = 0;
+
+        [DefaultValue(-1)]
+        [XmlElement("FrameToLoopOn")]
+        public int LoopOnFrame { get; set; } = -1;
+
+        [XmlElement("Direction")]
+        public SpriteAnimationDirectionData[] Directions { get; set; }
+
+        public void OnDeserialized()
         {
-            try
+            if (LoopOnFrame < -1)
+                throw new InvalidDataException("Bad field 'FrameToLoopOn' (must be a positive number or -1");
+
+            foreach (var direction in Directions.EmptyIfNull())
             {
-                var xmlData = buffer.XmlDeserialize<SpriteXmlData>();
-                foreach (var animation in xmlData.Animations)
-                {
-                    string animationName = animation.Name.CheckField("Name");
-                    string srcImage = animation.SrcImage.CheckField("SrcImage");
-                    int frameDelay = animation.FrameDelay.OptField(0);
-                    int frameToLoopOn = animation.FrameToLoopOn.OptField(-1);
+                direction.OnDeserialized();
 
-                    if (frameToLoopOn < -1)
-                        throw new InvalidDataException("Bad field 'FrameToLoopOn' (must be a positive number or -1");
-
-                    var directions = new List<SpriteAnimationDirectionData>();
-                    foreach (var direction in animation.Directions)
-                    {
-                        int x = direction.X.CheckField("X");
-                        int y = direction.Y.CheckField("Y");
-                        int frameWidth = direction.FrameWidth.CheckField("FrameWidth");
-                        int frameHeight = direction.FrameHeight.CheckField("FrameHeight");
-                        int originX = direction.OriginX.OptField(0);
-                        int originY = direction.OriginY.OptField(0);
-                        int numFrames = direction.NumFrames.OptField(1);
-                        int numColumns = direction.NumColumns.OptField(numFrames);
-
-                        if (numColumns < 1 || numColumns > numFrames)
-                            throw new InvalidDataException("Bad field 'NumColumns': must be between 1 and the number of frames");
-
-                        if (frameToLoopOn >= numFrames)
-                            throw new InvalidDataException("Bad field 'FrameToLoopOn': exceeds the number of frames");
-
-                        directions.Add(new SpriteAnimationDirectionData(
-                            new Point(x, y), 
-                            new Size(frameWidth, frameHeight),
-                            new Point(originX, originY),
-                            numFrames,
-                            numColumns));
-                    }
-
-                    if (_animations.ContainsKey(animationName))
-                        throw new InvalidDataException("Duplicate animation '{0}'".F(animationName));
-
-                    _animations.Add(animationName, new SpriteAnimationData(srcImage, directions, frameDelay, frameToLoopOn));
-
-                    // 첫번째 애니메이션을 기본으로 해줍니다
-                    if (_animations.Count == 1)
-                        DefaultAnimationName = animationName;
-                }
+                if (LoopOnFrame >= direction.NumFrames)
+                    throw new InvalidDataException("Bad field 'FrameToLoopOn': exceeds the number of frames");
             }
-            catch (Exception ex)
-            {
-                Debug.Error("Failed to load sprite: {0}".F(ex.Message));
-                return false;
-            }
-            return true;
         }
     }
 
     [XmlRoot("Sprite")]
-    public class SpriteXmlData
+    public class SpriteData : IXmlDeserialized
     {
-        public class AnimationDirection
-        {
-            public int? X { get; set; }
-            public int? Y { get; set; }
-            public int? FrameWidth { get; set; }
-            public int? FrameHeight { get; set; }
-            public int? OriginX { get; set; }
-            public int? OriginY { get; set; }
-            public int? NumFrames { get; set; }
-            public int? NumColumns { get; set; }
-
-            public bool OriginXSpecified { get { return OriginX != null; } }
-            public bool OriginYSpecified { get { return OriginY != null; } }
-            public bool NumFramesSpecified { get { return NumFrames != null; } }
-            public bool NumColumnsSpecified { get { return NumColumns != null; } }
-        }
-
-        public class Animation
-        {
-            [XmlAttribute]
-            public string Name { get; set; }
-            public string SrcImage { get; set; }
-            public int? FrameDelay { get; set; }
-            public int? FrameToLoopOn { get; set; }
-
-            [XmlElement("Direction")]
-            public AnimationDirection[] Directions { get; set; }
-
-            public bool FrameDelaySpecified { get { return FrameDelay != null; } }
-            public bool FrameToLoopOnSpecified { get { return FrameToLoopOn != null; } }
-        }
-
         [XmlElement("Animation")]
-        public Animation[] Animations { get; set; }
+        public SpriteAnimationData[] AnimationDatas { get; set; }
+
+        [XmlIgnore]
+        public IReadOnlyDictionary<string, SpriteAnimationData> Animations => _animations;
+
+        [XmlIgnore]
+        public string DefaultAnimationName { get; private set; }
+
+        Dictionary<string, SpriteAnimationData> _animations;
+
+        public void OnDeserialized()
+        {
+            AnimationDatas.Do(a => a.OnDeserialized());
+            _animations = AnimationDatas.ToDictionary(a => a.Name, a => a);
+
+            // 첫번째 애니메이션을 기본으로 해줍니다
+            DefaultAnimationName = AnimationDatas[0].Name;
+        }
     }
 }
